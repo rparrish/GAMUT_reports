3ab7f4799d5e1172c5babddc5e5a102e2f94cec7
Rollie Parrish <rparrish@flightweb.com>
2014-09-28 17:32:45 -0700

    new function to clean up the process

---
+++ R/gamutplot.R
@@ -0,0 +1,34 @@
+#'
+#' gamutplot
+#'
+#' returns the GAMUT dotplot and table
+#'
+#' @param title the main title for the plot
+#' @param mydata data frame with (group, numerator, denominator)
+#' @param reordering sort order for the table and plot
+#' @return a list - table has the raw data and plot has the dotplot
+#' @author Rollie Parrish <rollie.parrish@@ampa.org>
+#'
+
+gamutplot <- function(title = "test",
+                       mydata,
+                       reordering
+                       ) {
+
+
+    plotData <- mydata[mydata[,3] > 0, ]
+
+
+    results <- plot_metrics(title,
+        program_name = plotData$group,
+        num = plotData[,2],
+        den = plotData[,3],
+        reordering = reordering
+    )
+
+    results
+}
+
+
+
+
