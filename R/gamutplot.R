5011e7c53d6a4928ce5c8c4bbd5d7fcadfe4c2aa
Rollie Parrish <rparrish@flightweb.com>
2014-09-28 19:42:06 -0700

    added @export

--- R/gamutplot.R
+++ R/gamutplot.R
@@ -4,11 +4,11 @@
 #' returns the GAMUT dotplot and table
 #'
 #' @param title the main title for the plot
-#' @param mydata data frame with (group, numerator, denominator)
+#' @param mydata data frame with group, numerator, denominator
 #' @param reordering sort order for the table and plot
 #' @return a list - table has the raw data and plot has the dotplot
 #' @author Rollie Parrish <rollie.parrish@@ampa.org>
-#'
+#' @export
 
 gamutplot <- function(title = "test",
                        mydata,
