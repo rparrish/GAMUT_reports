4a35a712499312371a6e32b96901c89c80277b65
Rollie Parrish <rparrish@flightweb.com>
2015-10-15 20:45:04 -0700

    catching up

--- reports/2014 Q4/driver.R
+++ reports/2014 Q4/driver.R
@@ -11,21 +11,36 @@
 
 reports_data <- data.frame(
     #redcap_data_access_group = low_volume$redcap_data_access_group,
-    redcap_data_access_group = c("Akron Childrens"), # "Cincinnati Childrens"),
-    dm_email = c("rparrish@flightweb.com")#, "Hamilton.Schwartz@cchmc.org")
+    redcap_data_access_group = c("AIM Region 8"),
+    dm_email = c("rollie.parrish@ampa.org")
     )
+
+programs_with_total_patients <-
+    monthly_data %>%
+    group_by(redcap_data_access_group, program_name, ID) %>%
+    summarize(total_patients = sum(total_patients)) %>%
+    ungroup() %>%
+    arrange(total_patients)
+
 
 reports_data <-
     program_info %>%
     select(redcap_data_access_group, dm_email) %>%
-    unique()
-operator <- ""
+    unique() %>%
+    semi_join(programs_with_total_patients)
+
+
 
 ## Generate report for each data access group
+operator <- ""
+AIM_reports_data <- filter(reports_data, substr(redcap_data_access_group, 1, 3) == "AIM")
+
 for (dag in reports_data$redcap_data_access_group) {
     filename <- gsub(" ", "_", dag)
     print(filename)
-    knit2pdf("GAMUT_Summary_2014_Q1.Rnw", output=paste0('GAMUT_2014_', filename, '_v2.tex'))
+    knit2pdf("GAMUT_Summary_2015_Q2.Rnw", output=paste0('GAMUT_2015Q2_', filename, '.tex'))
+    ## cleanup
+    unlink(c("*.toc", "*.out", "*.tex",  "*.gz", "*.aux","*.log"))
 
 }
 
@@ -35,7 +50,7 @@ for (dag in reports_data$redcap_data_access_group) {
     operator = "AIM"
     filename <- gsub(" ", "_", "Air_Methods")
     print(filename)
-    knit2pdf("GAMUT_Summary_2014_Q1.Rnw", output=paste0('GAMUT_2014_', filename, '_v2.tex'))
+    knit2pdf("GAMUT_Summary_2015_Q2.Rnw", output=paste0('GAMUT_2015Q2_', filename, '.tex'))
 
 
 ### Send reports via Send-It
