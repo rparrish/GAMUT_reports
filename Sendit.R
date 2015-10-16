fd261b1693a6be3210ebc6e3222ba82cf94fb6b9
Rollie Parrish <rparrish@flightweb.com>
2015-10-15 20:44:04 -0700

    renamed to sendit_data

--- Sendit.R
+++ Sendit.R
@@ -6,12 +6,14 @@
 #'
 #'
 #'
+#'
+
+library(rjson)
+
 source(".REDCap_config.R") ## sendit username & password
 
 url <- "https://ampa.org/redcap/plugins/sendit_api/sendit.php"
-message  <- "This is a revision of the GAMUT database report for 2014 with two changes. First, the graph for `waveform capnography among pediatric patients` has been updated with the correct data fields. Second, this version is now correctly filtered to include only 2014 data.
-
-A WebEx conference call to discuss the report will take place on April 20th at 10:30 AM Eastern (7:30 AM Pacific). Connection information will be sent separately.
+message  <- "The GAMUT database report for Midyear 2015 is respectfully distributed for your review. This report includes data between July 2014 and June 2015.
 
 Thank you on behalf of the GAMUT database oversight committee."
 
@@ -41,22 +43,22 @@ no_data <-
     mutate("months submitted" = ifelse(is.na(n), 0, n))
 
 
-reports_data <- reports_data_second
+sendit_data <- reports_data#_second
 
-reports_data$file = gsub(" ", "_", reports_data$redcap_data_access_group)
+sendit_data$file = gsub(" ", "_", sendit_data$redcap_data_access_group)
 
 log <- list()
-for (row in 1:length(reports_data$dm_email)) {
+for (row in 1:length(sendit_data$dm_email)) {
     response <-  sendit(username=username, password=password,
-                        subject=paste("GAMUT 2014 Summary Report - ", reports_data$redcap_data_access_group[row]),
+                        subject=paste("GAMUT 2015 Mid-year Summary Report - ", sendit_data$redcap_data_access_group[row]),
                         message = message,
-                        recipient =  reports_data$dm_email[row],
+                        recipient =  sendit_data$dm_email[row],
                         expireDays = 15,
-                        file = paste0("GAMUT_2014_", reports_data$file[row], "_v2.pdf"),
+                        file = paste0("GAMUT_2015Q2_", sendit_data$file[row], ".pdf"),
                         url = url
     )
     log[[row]] <- fromJSON(response)
-    cat(paste("Sent to:", reports_data$dm_email[row]), "\n")
+    cat(paste("Sent to:", sendit_data$dm_email[row]), "\n")
     Sys.sleep(10)
 
 }
