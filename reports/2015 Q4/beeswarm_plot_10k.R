f8e4dca7700a50679d6b6d99cd26c105960b9ac7
rparrish <rparrish@flightweb.com>
2016-03-12 09:04:26 -0800

    added shadowtext function to improve readability of point labels

--- reports/2015 Q4/beeswarm_plot_10k.R
+++ reports/2015 Q4/beeswarm_plot_10k.R
@@ -10,7 +10,7 @@
 #' @export
 
 
-beeswarm_plot1k <- function(data = NULL, title = "GAMUT Metric title", ...)  {
+beeswarm_plot10k <- function(data = NULL, title = "GAMUT Metric title", ...)  {
     beeswarm_data <- filter(data, den >=5)
 
     indiv_data <-
@@ -19,7 +19,7 @@ beeswarm_plot1k <- function(data = NULL, title = "GAMUT Metric title", ...)  {
 
     program_rate_title <- " insufficient data"
 
-    scale_text <- " per 1000 "
+    scale_text <- " per 10000 "
 
 
     if(nrow(indiv_data) > 0) {
@@ -27,7 +27,7 @@ beeswarm_plot1k <- function(data = NULL, title = "GAMUT Metric title", ...)  {
             indiv_data %>%
             with(., prop.test(num, den))
 
-        program_rate_title <- paste0(round(indiv_points$estimate*1000,1),
+        program_rate_title <- paste0(round(indiv_points$estimate*10000,1),
                                 scale_text,
                                 " (", indiv_data$num, "/", indiv_data$den, ")")
     }
@@ -39,7 +39,7 @@ beeswarm_plot1k <- function(data = NULL, title = "GAMUT Metric title", ...)  {
     p <- {
         #plot(1, type="n", xlab="", ylab="", xlim=c(0, 1), xts = "", yts = "" )
 
-    beeswarm(beeswarm_data$num/beeswarm_data$den*1000,
+    beeswarm(beeswarm_data$num/beeswarm_data$den*10000,
              pch=16, horizontal = TRUE,
              #xaxt = "n",
              bty = "n",
@@ -47,7 +47,7 @@ beeswarm_plot1k <- function(data = NULL, title = "GAMUT Metric title", ...)  {
              #corralWidth = .25,
              main = "",
              xlab = paste0("\nGAMUT Overall: ",
-                           round(sum(data$num)/sum(data$den)*1000,1),
+                           round(sum(data$num)/sum(data$den)*10000,1),
                            scale_text, " (",
                            sum(data$num), "/",
                            sum(data$den), ")",
@@ -58,26 +58,26 @@ beeswarm_plot1k <- function(data = NULL, title = "GAMUT Metric title", ...)  {
 
     if(nrow(indiv_data) > 0) {
 
-     arrows(x0 = indiv_points$conf.int[1]*1000, y0=1.1, #1.3,
-            x1 = indiv_points$conf.int[2]*1000, y1=1.1, #1.3,
+     arrows(x0 = indiv_points$conf.int[1]*10000, y0=1.1, #1.3,
+            x1 = indiv_points$conf.int[2]*10000, y1=1.1, #1.3,
             code = 3, angle = 90, col = "blue",
             lwd = 2,
             length =.1)
 
     points(y = 1.1, #1.3,
-           x=indiv_points$estimate*1000, pch=19, cex = 1.2, col = "blue") # add mean
+           x=indiv_points$estimate*10000, pch=19, cex = 1.2, col = "blue") # add mean
 
     # add text labels
     shadowtext(x = indiv_points$estimate,
-               y = 1.30,
-               paste0("  ", round(indiv_points$estimate*100,1),scale_text),
-               col = "blue",
-               bg = "white",
-               cex = 1,
-               r = 0.2)
-    }
+         y = 1.30,
+         paste0("  ", round(indiv_points$estimate*100,1),scale_text),
+         col = "blue",
+         bg = "white",
+         cex = 1,
+         r = 0.2)
+ }
 
-    overall_mean <- sum(data$num)/sum(data$den)*1000
+    overall_mean <- sum(data$num)/sum(data$den)*10000
 
     # add mean
     segments(y0 = .6, x0=overall_mean,
