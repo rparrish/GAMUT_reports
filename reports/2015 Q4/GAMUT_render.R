fbb0daa582a33cd5771cd98da69d2c33491cb28d
rparrish <rparrish@flightweb.com>
2016-03-14 19:06:02 -0700

    set to pdf

--- reports/2015 Q4/GAMUT_render.R
+++ reports/2015 Q4/GAMUT_render.R
@@ -1,6 +1,6 @@
 
 
-GAMUT_render <- function(format = "html_document") {
+GAMUT_render <- function(format = "pdf_document") {
         program <- program_name
         filename <- gsub(" ", "_", program_name)
         dag <- program_name
@@ -9,7 +9,10 @@ GAMUT_render <- function(format = "html_document") {
                           output_format = format,
                           #intermediates_dir = "intermediate",
                           clean = TRUE,
-                          #envir = new.env(),
-                          output_file =paste0('GAMUT_2015Q4_', filename, '.html')
+                          envir = new.env(),
+                          output_file =paste0('GAMUT_2015Q4_', filename, '.pdf')
+
         )
+        # remove figures
+        unlink("figures/*.pdf")
 }
