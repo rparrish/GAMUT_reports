a59596b4136b61bae26dbb96d6a88aef9b5dad49
rparrish <rparrish@flightweb.com>
2016-08-28 06:36:54 -0700

    catching up from 2015 summary

--- Sendit.R
+++ Sendit.R
@@ -15,7 +15,7 @@ library(RCurl)
 source(".REDCap_config.R") ## sendit username & password
 
 url <- "https://ampa.org/redcap/plugins/sendit_api/sendit.php"
-message  <- "The GAMUT database report for Midyear 2015 is respectfully distributed for your review. This report includes data between July 2014 and June 2015.
+message  <- "The GAMUT database report for 2015 is respectfully distributed for your review. This report includes data between January to December 2015.
 
 Thank you on behalf of the GAMUT database oversight committee."
 
@@ -25,31 +25,26 @@ reports_data_all <- reports_data
 reports_data_AIM <-
     reports_data_all %>%
     filter(dm_email != "") %>%
-    filter(grepl("AIM", redcap_data_access_group))
+    filter(grepl("AIM", program_name))
 
 # AEL
 reports_data_AEL <-
     reports_data_all %>%
     filter(dm_email != "") %>%
-    filter(grepl("AEL", redcap_data_access_group))
+    filter(grepl("AEL", program_name))
 
 reports_data_second <-
     reports_data_all %>%
-    semi_join(monthly_data, by = "redcap_data_access_group") %>%
     filter(dm_email != "") %>%
-    filter(!grepl("AIM|AEL", redcap_data_access_group))
-
-no_data <-
-    reports_data_all %>%
-    filter(dm_email == "") %>%
-    left_join(record_counts) %>%
-    mutate("months submitted" = ifelse(is.na(n), 0, n))
+    filter(!grepl("AIM|AEL", program_name))
 
 
-sendit_data <- reports_data
+sendit_data <- reports_data_second
 
 sendit_data$file = gsub(" ", "_", sendit_data$program_name)
 
+
+start_time <- proc.time()
 log <- list()
 for (row in 1:length(sendit_data$dm_email)) {
     response <-  sendit(username=username, password=password,
@@ -65,10 +60,10 @@ for (row in 1:length(sendit_data$dm_email)) {
         "at", as.character(Sys.time()),"\n")
     Sys.sleep(10)
 }
+elapsed <- proc.time() - start_time
 
 sendit_log_AIM <- as.data.frame(do.call(rbind, log))
 sendit_log_AEL <- as.data.frame(do.call(rbind, log))
 sendit_log_second <- as.data.frame(do.call(rbind, log))
-sendit_log_FFL <- as.data.frame(do.call(rbind, log))
 
-sendit_log <- rbind(sendit_log_AEL, sendit_log_second, sendit_log_FFL)
+sendit_log <- rbind(sendit_log_AIM, sendit_log_second)
--- reports/2015 Q4/driver.R
+++ reports/2015 Q4/driver.R
@@ -19,15 +19,9 @@ end_date <- as.Date("2016-01-01")
 #' Testing
 reports_data <- data.frame(
     #redcap_data_access_group = low_volume$redcap_data_access_group,
-    program_name = c("Flight For Life CO",  "Cincinnati Childrens", "Akron Childrens"),
-    dm_email = c("rparrish@flightweb.com", "rollie.parrish@ampa.org", "rparrish@flightweb.com")
+    program_name = c("Cleveland Clinic Adult"),
+    dm_email = c("browna23@ccf.org")
     )
-
-#' send to larger group
-reports_data <-
-    program_info %>%
-    select(redcap_data_access_group, dm_email) %>%
-    unique()
 
 ## testing
 
@@ -36,7 +30,7 @@ reports_data <-
 }
 
 # Uncomment next line to get new data from REDCap
-#GAMUT_data("GAMUT.Rdata")
+GAMUT_data("GAMUT.Rdata")
 load("GAMUT.Rdata")
 
 ## time data
@@ -78,9 +72,22 @@ overview$total_contacts <-
     monthly_data %>%
     with(., sum(total_patients))
 
+programs_with_data <-
+    monthly_data %>%
+    select(program_name) %>%
+    unique()
+
+#' send to larger group
+reports_data <-
+    program_info %>%
+    select(program_name, dm_email) %>%
+    filter(dm_email != "") %>%
+    semi_join(programs_with_data) %>%
+    mutate(program_name = gsub("\\/", " ", program_name)) %>%
+    unique()
 
 
-
+start_time <- proc.time()
 ## Generate report for each program
 for (program_name in reports_data$program_name) {
         dag <- program_name
@@ -89,7 +96,7 @@ for (program_name in reports_data$program_name) {
 
     GAMUT_render(format = "pdf_document")
 }
-
+elapsed <- proc.time() - start_time
 
 ## Generate single report for Air Methods (operator = "AIM")
     dag = ""
