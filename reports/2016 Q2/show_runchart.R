065a6fbb53a8f69ee5b3d1ec091286260b6aab6b
rparrish <rparrish@flightweb.com>
2016-09-02 15:23:09 -0700

    initial commit or show_runchart

---
+++ reports/2016 Q2/show_runchart.R
@@ -0,0 +1,13 @@
+#' show_plotData.R
+#'
+#' @return runchart (qichart) the spedificed measure
+#' @export
+#'
+
+show_runchart <- function(measure, program_name) {
+
+     metric <- metric_data[metric_data$measure_id == measure,]
+
+     cat("runchart")
+}
+
