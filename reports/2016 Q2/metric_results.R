899a80316e8b02dafb35a6e5d80d96a3bfca66bd
rparrish <rparrish@flightweb.com>
2016-09-02 19:36:26 -0700

    added program to function calls

--- reports/2016 Q2/metric_results.R
+++ reports/2016 Q2/metric_results.R
@@ -1,24 +1,25 @@
 
-metric_results <- function(measure = "HT-1", show_plotData = FALSE, show_runchart = TRUE) {
+metric_results <- function(measure = "HT-1", show_plotData = FALSE, show_runchart = TRUE, program) {
 
     metric <- metric_data[metric_data$measure_id == measure,]
 
-    plotData <- pd(dag=dag,
+
+    plotData <- pd(dag=program,
                    num=metric$numerator_field,
                    den=metric$denominator_field
     )
-    beeswarm_plot(plotData, metric$full_name, program_name
+    beeswarm_plot(plotData, metric$full_name, program
     )
 
     # generate kable
     if(show_plotData) {
-        show_plotData(measure, program_name )
+        show_plotData(measure, program )
     }
 
 
     # generate kable
     if(show_runchart) {
-        show_runchart(measure, program_name )
+        show_runchart(measure, program )
     }
 
 
