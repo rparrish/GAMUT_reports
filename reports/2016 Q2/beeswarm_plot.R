1cddf8f1a8a6f765e764e8aaf60a1df2ec9042c0
rparrish <rparrish@flightweb.com>
2016-09-04 11:33:01 -0700

    added unit paramater

--- reports/2016 Q2/beeswarm_plot.R
+++ reports/2016 Q2/beeswarm_plot.R
@@ -10,7 +10,7 @@
 #' @export
 
 
-beeswarm_plot <- function(data = NULL, title = "GAMUT Metric title", program = NULL, plot_scale = 100, ...)  {
+beeswarm_plot <- function(data = NULL, title = "GAMUT Metric title", program = NULL, unit = "Percent",  ...)  {
     beeswarm_data <- filter(data, den >=5)
 
     indiv_data <-
@@ -19,7 +19,22 @@ beeswarm_plot <- function(data = NULL, title = "GAMUT Metric title", program = N
 
     program_rate_title <- " insufficient data"
 
-    scale_text <-  "% "
+    if(unit == "Percent") {
+        plot_scale = 100
+        plot_xlim  = 100
+        scale_text <-  "% "
+    }
+    if(unit == "per 1000") {
+        plot_scale = 1000
+        plot_xlim  = 100
+        scale_text <-  "  "
+    }
+
+    if(unit == "per 10000") {
+        plot_scale = 10000
+        plot_xlim = 1000
+        scale_text <-  ""
+    }
 
     if(nrow(indiv_data) > 0) {
         indiv_points <-
@@ -39,45 +54,47 @@ beeswarm_plot <- function(data = NULL, title = "GAMUT Metric title", program = N
     p <- {
         #plot(1, type="n", xlab="", ylab="", xlim=c(0, 1), xts = "", yts = "" )
 
-    beeswarm(beeswarm_data$num/beeswarm_data$den,
+    beeswarm(beeswarm_data$num/beeswarm_data$den*plot_scale,
              pch=16, horizontal = TRUE,
              xaxt = "n",
              bty = "n",
-             xlim = c(0,1),
+             xlim = c(0, plot_xlim),
              #corral = "random",
              #corralWidth = .25,
              main = "",
              xlab = paste0("\nGAMUT Overall: ",
-                           round(sum(data$num)/sum(data$den)*100,1),
+                           round(sum(beeswarm_data$num)/sum(beeswarm_data$den)*plot_scale,1),
                            scale_text, " (",
-                           sum(data$num), "/",
-                           sum(data$den), ")",
+                           sum(beeswarm_data$num), "/",
+                           sum(beeswarm_data$den), ")",
                            "\nProgram rate: ", program_rate_title),
-             method="hex",
-             ...
+             method="hex"
     )
 
     if(nrow(indiv_data) > 0) {
 
-    arrows(x0 = indiv_points$conf.int[1], y0=1.1, #1.3,
-           x1 = indiv_points$conf.int[2], y1=1.1, #1.3,
-           code = 3, angle = 90, col = "blue",
+    arrows(x0 = indiv_points$conf.int[1]*plot_scale, y0=1.1, #1.3,
+           x1 = indiv_points$conf.int[2]*plot_scale, y1=1.1, #1.3,
+           code = 3, angle = 90,
+           col = rgb(093, 165, 218, maxColorValue = 255),  #"blue",
            lwd = 2,
            length =.1)
     points(y = 1.1, #1.3,
-           x=indiv_points$estimate, pch=19, cex = 1.2, col = "blue") # add mean
+           x=indiv_points$estimate*plot_scale, pch=19, cex = 1.2,
+           col = rgb(093, 165, 218, maxColorValue = 255)  #"blue",
+           )# add mean
 
     # add text labels
-    shadowtext(x = indiv_points$estimate,
+    shadowtext(x = indiv_points$estimate*plot_scale,
          y = 1.30,
-         paste0("  ", round(indiv_points$estimate*100,1),scale_text),
-         col = "blue",
+         paste0("  ", round(indiv_points$estimate*plot_scale,1),scale_text),
+         col = rgb(093, 165, 218, maxColorValue = 255),  #"blue",
          bg = "white",
          cex = 1,
          r = 0.4)
  }
 
-    overall_mean <- sum(data$num)/sum(data$den)
+    overall_mean <- sum(beeswarm_data$num)/sum(beeswarm_data$den)*plot_scale
 
     # add mean
     segments(y0 = .6, x0=overall_mean,
@@ -87,12 +104,13 @@ beeswarm_plot <- function(data = NULL, title = "GAMUT Metric title", program = N
     }
 
     axis(1,
-            labels = paste0(pretty(c(0, data$num/data$den*100, 100)), "%"),
-            at     = pretty(c(0, data$num/data$den, 1)),
-            xlim   = c(0,1),
+            labels = paste0(pretty(c(0, beeswarm_data$num/beeswarm_data$den, plot_xlim)), scale_text),
+            at     = pretty(c(0, beeswarm_data$num/beeswarm_data$den, plot_xlim)),
+            xlim   = c(0, plot_xlim),
             las    = TRUE)
 
     title(main = list(paste(strwrap(title), collapse = "\n"), cex = 1,
-                      col = "red", font = 3))
+                      col = rgb(093, 165, 218, maxColorValue = 255),  #"blue",
+                      font = 3))
     invisible(p)
 }
