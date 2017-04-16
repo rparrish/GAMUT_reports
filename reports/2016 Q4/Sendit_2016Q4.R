d78ae06fd63e70e463e304135d39205972cc37e4
rparrish <rparrish@flightweb.com>
2017-04-16 15:40:37 -0700

    wrapping up 2016

--- reports/2016 Q4/Sendit_2016Q4.R
+++ reports/2016 Q4/Sendit_2016Q4.R
@@ -13,18 +13,19 @@ library(httr)
 library(RCurl)
 library(GAMUT)
 library(jsonlite)
+library(notifier)
 
 source(".REDCap_config.R") ## sendit username & password
 
 url <- "https://redcap.gamutqi.org/plugins/sendit_api/sendit.php"
-message  <- "The GAMUT database report for Midyear 2016 Q2 is respectfully distributed for your review. This report includes data between July 2015 to June 2016.
+message  <- "The GAMUT database report for 2016 is respectfully distributed for your review. This report includes data between July 2015 to June 2016.
 
 Thank you on behalf of the GAMUT database oversight committee."
 
-subject <- "GAMUT 2016 Q2 Summary Report"
-org_subject <- "GAMUT 2016 Q2 Organizational Summary Report"
+subject <- "GAMUT 2016 Q4 Summary Report"
+org_subject <- "GAMUT 2016 Q4 Organizational Summary Report"
 org_message <- "
-The GAMUT database reports for Midyear 2016 Q2 for your organization are respectfully distributed for your review. This version shows the metric performance for each of your organization's programs on a single report.
+The GAMUT database reports for 2016 for your organization are respectfully distributed for your review. This version shows the metric performance for each of your organization's programs on a single report.
 
 Thank you on behalf of the GAMUT database oversight committee.
 
@@ -38,57 +39,68 @@ reports_data_AEL <-
     reports_data_all %>%
     filter(dm_email != "") %>%
     filter(grepl("^AEL", program_name))
+sendit_data <- reports_data_AEL
 
 # AIM
 reports_data_AIM <-
     reports_data_all %>%
     filter(dm_email != "") %>%
     filter(grepl("^AIM", program_name))
+sendit_data <- reports_data_AIM
 
 # Mtr
 reports_data_MTr <-
     reports_data_all %>%
     filter(dm_email != "") %>%
     filter(grepl("^MTr", program_name))
+sendit_data <- reports_data_MTr
+
+# PHI
+reports_data_PHI <-
+    reports_data_all %>%
+    filter(dm_email != "") %>%
+    filter(grepl("^PHIr", program_name))
+sendit_data <- reports_data_PHI
 
 # Everyone else
 reports_data_main <-
     reports_data_all %>%
     filter(dm_email != "") %>%
-    filter(!grepl("^AIM|^AEL|^Mtr", program_name))
-
-
+    filter(!grepl("^AIM|^AEL|^MTr|^PHI", program_name))
 sendit_data <- reports_data_main
 
-sendit_data$file = gsub(" ", "_", sendit_data$program_name)
+## Larger organizations (AEL, AIM, MTr, PHI)
+# create a zip file and send just that
+#
 
 
 ## individual programs
 start_time <- proc.time()
+sendit_data$file = gsub(" ", "_", sendit_data$program_name)
 log <- list()
 for (row in 1:length(sendit_data$dm_email)) {
     #print(row)
 
     response <-  sendit(username=username, password=password,
-                        subject=paste("GAMUT 2016 Q2 Summary Report - ", sendit_data$program_name[row]),
+                        subject=paste("GAMUT 2016 Q4 Summary Report - ", sendit_data$program_name[row]),
                         message = message,
                         recipient =  sendit_data$dm_email[row],
                         expireDays = 15,
-                        file = paste0("GAMUT_2016Q2_", sendit_data$file[row], ".pdf"),
+                        file = paste0("GAMUT_2016Q4_", sendit_data$file[row], ".pdf"),
                         url = url
     )
 
        log[[row]] <- fromJSON(response)
     cat(paste("Sent", sendit_data$program_name[row], "to", sendit_data$dm_email[row]),
         "at", as.character(Sys.time()),"\n")
-    Sys.sleep(10)
+    Sys.sleep(5)
 }
 elapsed <- proc.time() - start_time
+notify(
+    title = "Send-it batch complete",
+    msg = c("Sent ", length(sendit_data$dm_email), " reports in ", elapsed[1], " minutes.")
+)
 
-
-## Larger organizations (AEL, AIM, MTr, PHI)
-# create a zip file and send just that
-#
 
 
 
